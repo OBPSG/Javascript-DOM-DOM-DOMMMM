6184795a5321f173b654ad93ee70eb029c420f4a
OBPSG <vbeasupergamer@gmail.com>
2022-02-09 09:32:22 -0600

    Got add div button working

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 let btn = document.createElement("button");
 btn.appendChild(document.createTextNode("Add Div"));
+btn.style.display = "block";
 document.body.appendChild(btn);
 btn.addEventListener("click", addDiv);
+
+function addDiv(event){
+    let div = document.createElement("div");
+    div.style.backgroundColor = "black";
+    div.style.height = "100px";
+    div.style.width = "100px";
+    div.style.display = "inline-block";
+    div.style.margin = "2px";
+    div.classList.add("square");
+    document.body.appendChild(div);
+}

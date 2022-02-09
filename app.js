5487cade524c1b1aa5c7179d670e3071b3265f1c
OBPSG <vbeasupergamer@gmail.com>
2022-02-09 13:54:28 -0600

    Added functions to show and hide the id of each div, and change its color randomly when clicked

--- app.js
+++ app.js
@@ -13,21 +13,45 @@ function addDiv(event){
     div.style.width = "100px";
     div.style.display = "inline-block";
     div.style.margin = "2px";
+    div.style.padding = "auto";
     div.classList.add("square");
     
     numDivsAdded += 1;
     div.id = numDivsAdded.toString();
     let idNode = document.createElement("div");
+    idNode.classList.add("idNode");
     idNode.appendChild(document.createTextNode(numDivsAdded.toString()));
     idNode.style.color = "white";
     idNode.style.height = "100px";
     idNode.style.fontSize = "32px";
-    idNode.style.display = "flex";
-    idNode.style.margin = "auto";
+    idNode.style.display = "none";
     idNode.style.justifyContent = "center";
     idNode.style.alignItems = "center";
     div.appendChild(idNode);
 
-    //div.addEventListener("mouseover", showId);
+    div.addEventListener("mouseenter", showId);
+    div.addEventListener("mouseleave", hideId);
+    div.addEventListener("click", setRandomColor);
     document.body.appendChild(div);
+}
+
+function showId(event)
+{
+    // console.log(event)
+    let target = event.target.querySelector(".idNode");
+    // console.log(target);
+    target.style.display = "flex";
+}
+
+function hideId(event)
+{
+    let target = event.target.querySelector(".idNode");
+    target.style.display = "none";
+}
+
+function setRandomColor(event)
+{
+    let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "saddlebrown"];
+    let index = Math.round(Math.random()*7);
+    event.target.style.backgroundColor = colors[index];
 }

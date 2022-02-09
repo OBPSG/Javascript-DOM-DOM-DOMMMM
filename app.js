693599931f4b71f68b4db28c35c3d70107ed135c
OBPSG <vbeasupergamer@gmail.com>
2022-02-09 10:14:00 -0600

    Added div to show the id of each square div.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var numDivsAdded = 0;
+
 let btn = document.createElement("button");
 btn.appendChild(document.createTextNode("Add Div"));
 btn.style.display = "block";
@@ -12,5 +14,20 @@ function addDiv(event){
     div.style.display = "inline-block";
     div.style.margin = "2px";
     div.classList.add("square");
+    
+    numDivsAdded += 1;
+    div.id = numDivsAdded.toString();
+    let idNode = document.createElement("div");
+    idNode.appendChild(document.createTextNode(numDivsAdded.toString()));
+    idNode.style.color = "white";
+    idNode.style.height = "100px";
+    idNode.style.fontSize = "32px";
+    idNode.style.display = "flex";
+    idNode.style.margin = "auto";
+    idNode.style.justifyContent = "center";
+    idNode.style.alignItems = "center";
+    div.appendChild(idNode);
+
+    //div.addEventListener("mouseover", showId);
     document.body.appendChild(div);
 }
